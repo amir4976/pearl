7c6dc46dc02269e5a47377c80650529c2ddcbbe9
amir4976 <amirhosain4976@gmail.com>
2025-03-30 16:33:24 +0330

    last fixing

--- src/components/serverActions/authenthication.action.ts
+++ src/components/serverActions/authenthication.action.ts
@@ -1,21 +1,28 @@
 import User from "@/models/User";
-import jwt,{JwtPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
 export async function authenthication() {
   const token = cookies().get("token")?.value;
+  const secret = process.env.JWT_SECRET;
 
-  const isValidToken = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+  if (!secret) {
+    throw new Error("JWT_SECRET is not defined in the environment variables");
+  }
+  if (!token) {
+    redirect("/");
+  }
+  const isValidToken = jwt.verify(token, secret) as JwtPayload;
 
   if (!isValidToken.email) {
-    redirect('/')
+    redirect("/");
   }
-  const findUser =await User.findOne({email:isValidToken.email})
-  if(!findUser){
-    redirect('/')
+  const findUser = await User.findOne({ email: isValidToken.email });
+  if (!findUser) {
+    redirect("/");
   }
-  if(findUser.role !== "ADMIN"){
-    redirect('/')
+  if (findUser.role !== "ADMIN") {
+    redirect("/");
   }
 }
